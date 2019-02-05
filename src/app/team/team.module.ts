4998182e893b223fb394fe6b7e5303c89f219797
Glen Coffey <glenrcoffey@gmail.com>
2019-02-05 14:08:11 +1100

    Initial code - bare Angular cli created app

---
+++ src/app/team/team.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+//import { AppRoutingModule } from './app-routing.module';
+
+import { TeamComponent } from './team.component';
+
+@NgModule({
+  declarations: [
+    TeamComponent
+  ],
+  imports: [
+    BrowserModule,
+  ],
+})
+export class TeamModule { }
